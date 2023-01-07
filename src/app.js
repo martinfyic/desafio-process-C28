71670072c90494c492580ca801f4e597255b319f
Martin Ferreira <martin.f.yic@gmail.com>
2023-01-07 08:43:38 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+import express from 'express';
+import session from 'express-session';
+import passport from 'passport';
+import { connectionDB } from './config/mongoDB.js';
+import { strategyLogin, strategySignup } from './middlewares/passportLocal.js';
+import routes from './routes/routes.js';
+
+const app = express();
+
+passport.use('login', strategyLogin);
+passport.use('signup', strategySignup);
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+	session({
+		secret: 'keyboard cat',
+		cookie: {
+			httpOnly: false,
+			secure: false,
+			maxAge: process.env.EXPIRATION_TIME,
+		},
+		rolling: true,
+		resave: true,
+		saveUninitialized: false,
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/ecommerce', routes);
+
+await connectionDB();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () =>
+	console.log(`  🚀 Servidor Ok ==> http://localhost:${PORT}/ecommerce/`)
+);
---
+++ src/config/mongoDB.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+export const connectionDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URL, {}, () => {
+			console.log(` 💽 Conectado a MongoDB Cloud`);
+		});
+	} catch (error) {
+		console.log(` ⚠️ Error ==> ${error?.message}`);
+		throw new Error('Error al conectarse a MongoDB Cloud');
+	}
+};
