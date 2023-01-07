802543ac783fdf13446e2b120cafa72fdc928ae7
Martin Ferreira <martin.f.yic@gmail.com>
2023-01-07 11:14:43 -0300

    remove console.logs

--- src/middlewares/passportLocal.js
+++ src/middlewares/passportLocal.js
@@ -11,23 +11,19 @@ passport.deserializeUser((id, done) => {
 	User.findById(id, done);
 });
 
-//-------LOGIN-------//
 export const strategyLogin = new Strategy((username, password, done) => {
 	User.findOne({ username }, (err, user) => {
 		if (err) return done(err);
 		if (!user) {
-			console.log(` ⚠️ Usuario no encontrado: ${username}`);
 			return done(null, false);
 		}
 		if (!isValidPassword(user, password)) {
-			console.log(' ⚠️ Contraseña invalida');
 			return done(null, false);
 		}
 		return done(null, user);
 	});
 });
 
-//-------SINGUP-------//
 export const strategySignup = new Strategy(
 	{
 		passReqToCallback: true,
@@ -35,26 +31,21 @@ export const strategySignup = new Strategy(
 	(req, username, password, done) => {
 		User.findOne({ username: username }, function (err, user) {
 			if (err) {
-				console.log(` ⚠️ Error in SingUp: ${err}`);
 				return done(err);
 			}
 
 			if (user) {
-				console.log(' ⚠️ El usuario ya existe');
 				return done(null, false);
 			}
 
 			const newUser = {
-				name: req.body.name,
 				username: req.body.username,
 				password: createHash(password),
 			};
 			User.create(newUser, (err, userWithId) => {
 				if (err) {
-					console.log(` ⚠️ Error al guardar el usuario: ${err}`);
 					return done(err);
 				}
-				console.log(' ✅ El usuario se registró correctamente');
 				return done(null, userWithId);
 			});
 		});
