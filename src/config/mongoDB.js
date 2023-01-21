9b68dcfd975b703b2ae3abc90a02a6611abdc627
Martin Ferreira <martin.f.yic@gmail.com>
2023-01-21 16:09:54 -0300

    add colors in console.logs

--- src/config/mongoDB.js
+++ src/config/mongoDB.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
+import 'colors';
 
 export const connectionDB = async () => {
 	try {
 		mongoose.connect(process.env.MONGO_URL, {}, () => {
-			console.log(` 💽 Conectado a MongoDB Cloud`);
+			console.log(` 💽 Conectado a MongoDB Cloud`.yellow);
 		});
 	} catch (error) {
-		console.log(` ⚠️ Error ==> ${error?.message || error}`);
+		console.log(` ⚠️ Error ==> ${error?.message || error}`.red);
 		throw new Error('Error al conectarse a MongoDB Cloud');
 	}
 };
